d386e32add4bc82b35f99bd738e07228a0dbaf33
JanikSpies <j.spies2020@gmail.com>
2025-12-31 14:12:13 +0100

    feat: add base styling config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import './App.css'
 import Button from './components/Button'
 
 function App() {
 
   return (
-    <>
-      HOCR Visualizer
+    <div className="py-6 px-2 md:px-4">
+      <h1 className="text-center">HOCR Visualizer</h1>
       <Button label="Press me" />
       <Button variant="secondary" label="Press me" />
-    </>
+    </div>
   )
 }
 
