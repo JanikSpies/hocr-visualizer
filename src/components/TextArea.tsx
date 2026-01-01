1b16d57d14fbf158453cba404702aad03bf62677
JanikSpies <j.spies2020@gmail.com>
2026-01-01 15:25:18 +0100

    feat: add textarea for hocr string

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,38 @@
+interface Props {
+    label?: string
+    placeholder?: string
+    error?: string
+    required?: boolean
+    value: string
+    setValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+const TextArea: React.FC<Props> = ({
+    label,
+    placeholder,
+    error,
+    required = true,
+    value,
+    setValue
+}) => {
+    return (
+        <label className="form-control w-full">
+            {label &&
+                <div>
+                    {label} {required && <span className="text-violet-600">*</span>}
+                </div>
+            }
+            <textarea
+                value={value}
+                placeholder={placeholder}
+                onChange={(e) => setValue(e.target.value)}
+                className={`p-4 rounded-2xl border border-slate-300 w-full h-full`}
+            />
+            <div className="label">
+                <span className="label-text-alt text-red-700">{error}</span>
+            </div>
+        </label>
+    )
+}
+
+export default TextArea;
