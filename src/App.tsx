fe446209fae673f8b1aab8f77682417102202ce1
JanikSpies <j.spies2020@gmail.com>
2026-01-01 15:13:52 +0100

    feat: add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
 
   return (
     <>
       HOCR Visualizer
+      <Button label="Press me" />
+      <Button variant="secondary" label="Press me" />
     </>
   )
 }
